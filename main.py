2ec10860f1186385ab57230e35a6fb55457093a7
chenkang <1573918344@qq.com>
2019-04-20 12:12:14 +0800

    2019年4月20日09:51:35

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import urllib
-from bs4  import  BeautifulSoup
-if __name__ == '__main__':
 
-    url ='https://www.meipai.com/'
-    urllib.request
-    response = urllib.urlopen('http://python.org/')
-    result =response.read().decode('utf-8')
-    print  result
+#coding=utf-8
+import urllib
+from bs4 import  BeautifulSoup
+
+def getHtml(url):
+  # urllib.addheaders('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36')
+   response = urllib.urlopen(url)
+   print response
+   soup =BeautifulSoup(response,'html.parser')
+   return response
+
+
+
+if __name__ == '__main__':
+    getHtml('https://www.meipai.com/')
+
 
 
